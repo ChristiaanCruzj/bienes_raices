44bda0703108670ce21283c4d8e83c2a1b02a1aa
ChristiaanCruzj <christiaancruzj@gmail.com>
2023-08-03 15:17:58 -0600

    burger responsive

---
+++ src/js/app.js
@@ -0,0 +1,16 @@
+document.addEventListener('COMContentLoaded', function() {
+
+    addEventListener();
+});
+
+function addEventListener() {
+    const mobileMenu = document.querySelector('.mobile-menu');
+
+    mobileMenu.addEventListener('click',function() {
+
+    })
+}
+
+function navegacionResponsive() {
+    console.log('desde navegacionResponsive');
+}
