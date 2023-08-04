2d009961b5f2808688e594697f7d662d0fe39345
ChristiaanCruzj <christiaancruzj@gmail.com>
2023-08-04 16:41:54 -0600

    add boton darkMode in navegacion.scss

--- src/js/app.js
+++ src/js/app.js
@@ -1,14 +1,30 @@
+//cuando el documento este listo se van a ejecutar estas funciones
 document.addEventListener('DOMContentLoaded', function() {
 
     addEventListener();
+
+    darkMode();
 });
+//funcion de darkMode
+function darkMode() {
+    const botonDarkMode = document.querySelector('.dark-mode-boton');
+    botonDarkMode.addEventListener(click, function () {
+
+    });
+}
 
 function addEventListener() {
     const mobileMenu = document.querySelector('.mobile-menu');
 
-    mobileMenu.addEventListener('click', navegacionResponsive)
+    mobileMenu.addEventListener('click', navegacionResponsive) //pude hacer ahi mismo mi funcion pero decidi sacarla como ejemplo
 }
 
 function navegacionResponsive() {
-    console.log('desde navegacionResponsive');
+    const navegacion = document.querySelector('.navegacion');
+    // if(navegacion.classList.contains('mostrar')) {
+    //     navegacion.classList.remove('mostrar');
+    // } else {
+    //     navegacion.classList.add('mostrar');
+    // }
+    navegacion.classList.toggle('mostrar');
 }
